08cd487a3400092ae8ffc1cc9562561559e836bf
haloandrei <haloandrei@gmail.com>
2024-08-17 21:45:23 +0300

    feat: add Viruses

--- Scripts/DNAView.gd
+++ Scripts/DNAView.gd
@@ -1,12 +1,12 @@
 extends Node
 
 @export var mob_scene: PackedScene
-
+@export var DNA_scene: NodePath
 
 func _on_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
-
+	
 	# Choose a random location on the SpawnPath.
 	# We store the reference to the SpawnLocation node.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
@@ -14,6 +14,10 @@ func _on_timer_timeout() -> void:
 	mob_spawn_location.progress_ratio = randf()
 
 	var player_position = Vector3(0,0,0);
+	var character = get_node(DNA_scene)
+	if character:
+		player_position = character.global_transform.origin
+		
 	mob.initialize(mob_spawn_location.position, player_position)
 
 	# Spawn the mob by adding it to the Main scene.
