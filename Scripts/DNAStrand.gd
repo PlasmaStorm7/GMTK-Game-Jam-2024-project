b099566782b17cd04f58310f88b3458650a32f71
haloandrei <haloandrei@gmail.com>
2024-08-18 18:05:26 +0300

    feat: spin upward

--- Scripts/DNAStrand.gd
+++ Scripts/DNAStrand.gd
@@ -5,7 +5,7 @@ extends RigidBody3D
 @export var cylinder_color1 = Color(0, 1, 0)
 @export var sphere_color2 = Color(1, 0, 0)
 @export var cylinder_color2 = Color(0, 1, 0)
-@export var spin_speed = 1.0
+@export var spin_speed = 0.6
 @export var velocity_strength = 5.0
 
 
@@ -21,8 +21,14 @@ func _ready():
 	cylinder2.material_override = create_material(cylinder_color2)
 	sphere2.material_override = create_material(sphere_color2)
 	
-	angular_velocity = Vector3(0, spin_speed, 0)
-	linear_velocity = transform.basis.y * velocity_strength
+	#rotation_degrees.z = 40
+	
+func _process(delta):
+	rotate_y(spin_speed * delta)
+	var move_dir = transform.basis.y.normalized() * velocity_strength * delta
+	translate(move_dir)
+	
+
 
 func create_material(color: Color):
 	var material = StandardMaterial3D.new()
